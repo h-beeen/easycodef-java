e11a821438b76495fd24c7efc63fb30591ea0f6f
kdhdd <kdso10@naver.com>
2025-11-25 16:05:21 +0900

    EasyCodefProperties 접근제어자 수정

--- src/main/java/io/codef/api/EasyCodefProperties.java
+++ src/main/java/io/codef/api/EasyCodefProperties.java
@@ -15,41 +15,25 @@ public class EasyCodefProperties {
 	private String publicKey 	= "";
 
 
-	public void setClientInfo(String clientId, String clientSecret) {
+	protected void setClientInfo(String clientId, String clientSecret) {
 		this.clientId = clientId;
 		this.clientSecret = clientSecret;
 	}
 
-	public void setClientInfoForDemo(String demoClientId, String demoClientSecret) {
+	protected void setClientInfoForDemo(String demoClientId, String demoClientSecret) {
 		this.demoClientId = demoClientId;
 		this.demoClientSecret = demoClientSecret;
 	}
 
-	public String getDemoClientId() {
-		return demoClientId;
-	}
-
-	public String getDemoClientSecret() {
-		return demoClientSecret;
-	}
-
-	public String getClientId() {
-		return clientId;
-	}
-
-	public String getClientSecret() {
-		return clientSecret;
-	}
-
-	public String getPublicKey() {
+	protected String getPublicKey() {
 		return publicKey;
 	}
 
-	public void setPublicKey(String publicKey) {
+	protected void setPublicKey(String publicKey) {
 		this.publicKey = publicKey;
 	}
 
-    public boolean checkClientInfo(EasyCodefServiceType serviceType) {
+    protected boolean checkClientInfo(EasyCodefServiceType serviceType) {
         if(serviceType.isApiService()) {
             if(clientId == null || clientId.trim().isEmpty()) {
                 return true;
@@ -63,15 +47,15 @@ public class EasyCodefProperties {
         }
     }
 
-    public boolean checkPublicKey() {
+    protected boolean checkPublicKey() {
         return publicKey == null || publicKey.trim().isEmpty();
     }
 
-    public String getClientIdByServiceType(EasyCodefServiceType serviceType) {
+    protected String getClientIdByServiceType(EasyCodefServiceType serviceType) {
         return serviceType.isApiService() ? clientId : demoClientId;
     }
 
-    public String getClientSecretByServiceType(EasyCodefServiceType serviceType) {
+    protected String getClientSecretByServiceType(EasyCodefServiceType serviceType) {
         return serviceType.isApiService() ? clientSecret : demoClientSecret;
     }
 }
