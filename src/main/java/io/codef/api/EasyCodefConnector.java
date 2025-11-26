23e8de8dc0eaa058015b95bcb144eb7b78c181f1
kdhdd <kdso10@naver.com>
2025-11-26 09:06:50 +0900

    Singleton 기반 HttpClientFactory 설계 적용

--- src/main/java/io/codef/api/EasyCodefConnector.java
+++ src/main/java/io/codef/api/EasyCodefConnector.java
@@ -3,7 +3,7 @@ package io.codef.api;
 import io.codef.api.dto.EasyCodefResponse;
 import io.codef.api.dto.HttpResponse;
 import io.codef.api.http.HttpClient;
-import io.codef.api.http.ApacheHttpClient;
+import io.codef.api.http.HttpClientFactory;
 import io.codef.api.http.HttpRequestBuilder;
 
 import static io.codef.api.ResponseHandler.handleErrorResponse;
@@ -12,11 +12,10 @@ import static io.codef.api.error.EasyCodefError.LIBRARY_SENDER_ERROR;
 
 public class EasyCodefConnector {
 
-    private static final HttpClient httpClient = new ApacheHttpClient();
-
     private EasyCodefConnector() {}
 
     protected static EasyCodefResponse execute(HttpRequestBuilder builder) {
+        HttpClient httpClient = HttpClientFactory.getInstance();
         HttpResponse httpResponse = httpClient.postJson(
                 builder.getUrl(),
                 builder.getHeaders(),
