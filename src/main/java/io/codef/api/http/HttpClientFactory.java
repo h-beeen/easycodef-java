c6fed7bf170c37e741b4a6db8ab1e4b398b2d907
kdhdd <kdso10@naver.com>
2025-11-28 10:50:53 +0900

    Implement Connection Pooling

--- src/main/java/io/codef/api/http/HttpClientFactory.java
+++ src/main/java/io/codef/api/http/HttpClientFactory.java
@@ -1,5 +1,11 @@
 package io.codef.api.http;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import static io.codef.api.constants.HttpConstant.*;
+
 public class HttpClientFactory {
     private static volatile HttpClient instance;
 
@@ -7,10 +13,26 @@ public class HttpClientFactory {
         if (instance == null) {
             synchronized (HttpClientFactory.class) {
                 if (instance == null) {
-                    instance = new ApacheHttpClient();
+                    instance = createPooledHttpClient();
                 }
             }
         }
         return instance;
+    }
+
+    private static HttpClient createPooledHttpClient() {
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(WAIT_CONNECTION_TIMEOUT)
+                .setConnectTimeout(CONNECT_TIMEOUT)
+                .setSocketTimeout(REAT_TIMEOUT)
+                .build();
+
+        CloseableHttpClient pooledClient = HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .setMaxConnTotal(MAX_CONNECTIONS)
+                .setMaxConnPerRoute(MAX_CONNECTIONS)
+                .build();
+
+        return new HttpClient(pooledClient);
     }
 }
