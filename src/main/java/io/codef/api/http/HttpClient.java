75d0cf2154145bf036bfee519fd36b281214e223
kdhdd <kdso10@naver.com>
2025-11-28 10:50:23 +0900

    인터페이스 제거

--- src/main/java/io/codef/api/http/HttpClient.java
+++ src/main/java/io/codef/api/http/HttpClient.java
@@ -1,8 +1,31 @@
 package io.codef.api.http;
 
 import io.codef.api.dto.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
 
-public interface HttpClient {
-    HttpResponse postJson (HttpUriRequest request);
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class HttpClient {
+
+    private final CloseableHttpClient httpClient;
+
+    public HttpClient(CloseableHttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
+    public HttpResponse postJson(HttpUriRequest request) {
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            int statusCode = response.getStatusLine().getStatusCode();
+            String responseBody =
+                    response.getEntity() == null ? "" :
+                            EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+            return new HttpResponse(statusCode, responseBody);
+        } catch (IOException e) {
+            return new HttpResponse(-1, e.getMessage());
+        }
+    }
 }
