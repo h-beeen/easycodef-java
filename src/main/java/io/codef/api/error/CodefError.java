7eb46e5d9301738020ca619906def2f430a714f0
kdhdd <kdso10@naver.com>
2025-12-01 09:27:39 +0900

    ErrorCode 문구 영문화 적용

--- src/main/java/io/codef/api/error/CodefError.java
+++ src/main/java/io/codef/api/error/CodefError.java
@@ -1,53 +1,27 @@
 package io.codef.api.error;
 
 public enum CodefError {
-    INVALID_JSON(
-            "json형식이 올바르지 않습니다."
-    ),
-    INVALID_PARAMETER(
-            "요청 파라미터가 올바르지 않습니다."
-    ),
     UNSUPPORTED_ENCODING(
-            "지원하지 않는 형식으로 인코딩된 문자열입니다."
-    ),
-
-    EMPTY_CLIENT_INFO(
-            "상품 요청을 위해서는 클라이언트 정보가 필요합니다. 클라이언트 아이디와 시크릿 정보를 설정하세요."
+            "The string is encoded in an unsupported format."
     ),
 
     PARSE_ERROR(
-            "클라이언트가 서버 응답을 기대한 형식대로 파싱하지 못했습니다. 요청 형식을 확인해 주세요."
+            "The client failed to parse the server response in the expected format. Please verify the request format."
     ),
 
     RSA_ENCRYPTION_ERROR(
-            "RSA 암호화 과정에서 오류가 발생했습니다. PublicKey 값을 확인해 주세요."
+            "An error occurred during RSA encryption. Please verify the PublicKey value."
     ),
 
     INVALID_2WAY_INFO(
-            "2WAY 요청 처리를 위한 정보가 올바르지 않습니다. 응답으로 받은 항목을 그대로 2way요청 항목에 포함해야 합니다."
+            "Invalid information for 2-way request processing. Items received in the response must be included exactly in the 2-way request."
     ),
     INVALID_2WAY_KEYWORD(
-            "추가 인증(2Way)을 위한 요청은 requestCertification메서드를 사용해야 합니다."
+            "Requests for additional authentication (2-way) must use the requestCertification method."
     ),
 
     INVALID_PATH_REQUESTED(
             "The path should be requested in the following format: `/v1/kr/***/***/...`"
-    ),
-
-    BAD_REQUEST(
-            "클라이언트 요청 오류로 인해 요청을 처리 할 수 ​​없습니다."
-    ),
-    UNAUTHORIZED(
-            "요청 권한이 없습니다."
-    ),
-    FORBIDDEN(
-            "잘못된 요청입니다."
-    ),
-    NOT_FOUND(
-            "요청하신 페이지(Resource)를 찾을 수 없습니다."
-    ),
-    METHOD_NOT_ALLOWED(
-            "요청하신 방법(Method)이 잘못되었습니다."
     ),
 
     EMPTY_SERVICE_TYPE(
@@ -71,10 +45,7 @@ public enum CodefError {
     ),
 
     IO_ERROR(
-            "통신 요청에 실패했습니다. IP: 211.55.34.5, PORT: 443 방향의 Outbound 포트가 열려 있는지 확인해 주세요."
-    ),
-    SERVER_ERROR(
-            "서버 처리중 에러가 발생 했습니다. 관리자에게 문의하세요."
+            "An error occurred because the request was either not sent properly or not received. Please check if the outbound port to IP: 211.55.34.5, PORT: 443 is open."
     );
 
     private final String message;
