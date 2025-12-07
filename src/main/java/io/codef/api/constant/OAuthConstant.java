6cb7998dd42b949814fd872427570e763981f0cf
kdhdd <kdso10@naver.com>
2025-12-07 16:54:49 +0900

    OAuthConstant (Javadoc)

--- src/main/java/io/codef/api/constant/OAuthConstant.java
+++ src/main/java/io/codef/api/constant/OAuthConstant.java
@@ -1,5 +1,11 @@
 package io.codef.api.constant;
 
+/**
+ * CODEF OAuth 인증 응답 처리 필드 키 enum 클래스
+ *
+ * @author kdso10
+ * @since Dec 5, 2025
+ */
 public enum OAuthConstant {
 	ACCESS_TOKEN("access_token"),
 	EXPIRES_IN("expires_in");
