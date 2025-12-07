4ea83335732ecdd7a12c5faa3844f6b2353bcde7
kdhdd <kdso10@naver.com>
2025-12-07 16:53:29 +0900

    CodefPath (Javadoc)

--- src/main/java/io/codef/api/constant/CodefPath.java
+++ src/main/java/io/codef/api/constant/CodefPath.java
@@ -1,5 +1,11 @@
 package io.codef.api.constant;
 
+/**
+ * CODEF 요청 경로 정의 클래스
+ *
+ * @author kdso10
+ * @since Dec 5, 2025
+ */
 public final class CodefPath {
 
 	public static final String GET_TOKEN = "/oauth/token?grant_type=client_credentials&scope=read";
