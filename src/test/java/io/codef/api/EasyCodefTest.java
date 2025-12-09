11b24977e73a77abbb84170cee38aee43bd6c7cf
happybean <happybean@hecto.co.kr>
2025-12-09 13:52:22 +0900

    devops: CI Test

--- src/test/java/io/codef/api/EasyCodefTest.java
+++ src/test/java/io/codef/api/EasyCodefTest.java
@@ -1,5 +1,8 @@
 package io.codef.api;
 
+import com.alibaba.fastjson2.JSONObject;
+import io.codef.api.error.CodefException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -11,6 +14,10 @@ class EasyCodefTest {
         EasyCodef easyCodef = new EasyCodef();
         easyCodef.setClientInfo("ef27cfaa-10c1-4470-adac-60ba476273f9", "83160c33-9045-4915-86d8-809473cdf5c3");
         easyCodef.setPublicKey("publicKey");
-        String s = easyCodef.requestProduct("/", EasyCodefServiceType.API, new HashMap<>());
+
+        Assertions.assertThrows(
+                CodefException.class,
+                () -> easyCodef.requestProduct("/api", EasyCodefServiceType.API, new HashMap<>())
+        );
     }
 }
